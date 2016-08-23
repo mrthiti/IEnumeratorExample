5205738af064883797047dcda2f04cc781f919be
Thiti Yamsung <thiti.59h@gmail.com>
2016-08-24 00:26:18 +0700

    First commit.
    Signed-off-by: Thiti Yamsung <thiti.59h@gmail.com>

---
+++ IEnumeratorExample/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumeratorExample {
+    class Program {
+        static void Main(string[] args) {
+
+            Month objMonth = new Month();
+
+            foreach (string item in objMonth) {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+    class Month : IEnumerable {
+
+        public string[] arrMonth = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+        public IEnumerator GetEnumerator() {
+            return new AllMonth(this);
+        }
+    }
+
+    class AllMonth : IEnumerator {
+
+        private Month _month;
+        private int index = -1;
+
+        public AllMonth(Month inMonth) {
+            _month = inMonth;
+        }
+
+        public object Current {
+            get {
+                return _month.arrMonth[index];
+            }
+        }
+
+        public bool MoveNext() {
+            index++;
+            if (index < _month.arrMonth.Length) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public void Reset() {
+            index = -1;
+        }
+    }
+}
